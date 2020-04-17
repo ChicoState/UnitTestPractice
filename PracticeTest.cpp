7f9f85e12197275d7a93ea2181d3d9bd183284fe
kbuffardi <kbuffardi@csuchico.edu>
2020-04-17 16:13:33 -0700

    Remove faulty test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,10 +20,3 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
-
-TEST(PracticeTest, is_not_a_palindrome)
-{
-    Practice obj;
-    bool actual = obj.isPalindrome("Ba");
-    ASSERT_TRUE(actual);
-}
