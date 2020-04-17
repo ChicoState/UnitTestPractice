cae7a9ebd20cc2a0a33ae32a2cefb7d6c7c73505
kbuffardi <kbuffardi@csuchico.edu>
2020-04-17 15:06:07 -0700

    Fix test scaffold function names

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -8,8 +8,8 @@
 class PracticeTest : public ::testing::Test
 {
 	protected:
-		RandoTest(){} //constructor runs before each test
-		virtual ~RandoTest(){} //destructor cleans up after tests
+		PracticeTest(){} //constructor runs before each test
+		virtual ~PracticeTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
