60c794b3f95e9f7db1e7d58e01fec0d9d94b4a77
aswingler1 <aswingler@mail.csuchico.edu>
2020-04-22 12:53:36 -0700

    test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -17,7 +17,7 @@ class PracticeTest : public ::testing::Test
 TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("aa");
+    bool actual = obj.isPalindrome("Aba");
     ASSERT_TRUE(actual);
 }
 
