233cb425b5a08a0dc96438fc33f6850b0a40c513
aswingler1 <36062866+aswingler1@users.noreply.github.com>
2020-04-27 12:38:39 -0700

    Update Practice.h

--- Practice.h
+++ Practice.h
@@ -8,7 +8,7 @@ using std::string;
 class Practice
 {
 public:
-  void sortDescending(int & first, int & second, int & third);
+  bool sortDescending(int & first, int & second, int & third);
   bool isPalindrome(string input);
 };
 
