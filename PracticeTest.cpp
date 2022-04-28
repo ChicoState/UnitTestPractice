cc19932a89e846f74cab865f24fe85d7941a615b
braulio <bviveros@mail.csuchico.edu>
2022-04-27 21:54:39 -0700

    added test and passing, still needs more test and extra function

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "Practice.h"
+#include <string>
 
 class PracticeTest : public ::testing::Test
 {
@@ -18,3 +19,31 @@ TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
 }
+
+TEST(PracticeTest, zzzrepeats)
+{
+    Practice testObject;
+    ASSERT_EQ( testObject.count_starting_repeats("zzz"), 3);
+}
+
+TEST(PracticeTest, descending_true)
+{
+    Practice testObject;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    testObject.sortDescending(a, b, c);
+    ASSERT_EQ(a, 3);
+    ASSERT_EQ(b, 2);
+    ASSERT_EQ(c, 1);
+}
+
+TEST(PracticeTest, isPalindrome)
+{
+    Practice testObject;
+    string palin = "tacocat";
+    bool isPalin = testObject.isPalindrome(palin);
+    ASSERT_EQ(isPalin, true);
+}
+
+TEST(PracticeTest, allnighter)
