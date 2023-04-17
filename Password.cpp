982f728885865ea0c0931f4a1eaeef89f9ace244
cmkoschnick <cmkoschnick@gmail.com>
2023-04-17 12:11:07 -0700

    added upper/lower

--- Password.cpp
+++ Password.cpp
@@ -13,9 +13,38 @@ int Password::count_leading_characters(string phrase){
   int repetition = 1;
   int index = 0;
 
+  if(phrase.length() == 0)
+  {
+	return 0;
+  }
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
     repetition++;
     index++;
   }
   return repetition;
+}
+
+
+bool Password::has_mixed_case(string phrase)
+{
+	int lower = 0;
+	int upper = 0;
+	int index = 0;
+	
+	while(index < phrase.length())
+	{
+		if(isupper(phrase[index]))
+		{
+			upper = 1;
+		}
+		else
+		{
+			lower = 1;
+		}
+		index ++;
+	}
+
+	return (upper && lower);
+
+	
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -24,3 +24,101 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1,actual);
 }
+
+//empty string
+TEST(PasswordTest, empty_string_test)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("");
+        ASSERT_EQ(0,actual);
+}
+
+//single letter lowercase
+TEST(PasswordTest, single_letter_lowercase)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("a");
+        ASSERT_EQ(1,actual);
+}
+
+//single character number
+TEST(PasswordTest, single_number_password)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("6");
+        ASSERT_EQ(1,actual);
+}
+
+//single punctuation
+TEST(PasswordTest, single_punctuation_password)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("?");
+        ASSERT_EQ(1,actual);
+}
+
+//multiple leading letters
+TEST(PasswordTest, multiple_leading_letters)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("bbc");
+        ASSERT_EQ(2,actual);
+}
+
+//multiple leading numbers
+TEST(PasswordTest, multiple_leading_numbers)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("111c");
+        ASSERT_EQ(3,actual);
+}
+
+//multiple leading punctuation
+TEST(PasswordTest, multiple_leading_punct)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("////0n");
+        ASSERT_EQ(4,actual);
+}
+
+//multiple spaces
+TEST(PasswordTest, multiple_spaces)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("   ");
+        ASSERT_EQ(3,actual);
+}
+
+//upper then lower
+TEST(PasswordTest, upper_then_lowercase)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("AAaa");
+        ASSERT_EQ(2,actual);
+}
+
+//longer string of same
+TEST(PasswordTest, multiple_same_character)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("aaaaaaaaaaaa");
+        ASSERT_EQ(12,actual);
+}
+
+//null terminator test
+TEST(PasswordTest, null_terminator_test)
+{
+    Password my_password;
+        int actual = my_password.count_leading_characters("\0");
+        ASSERT_EQ(0,actual);
+}
+
+TEST(PasswordTest, has_upper_and_lower){
+	Password obj;
+	ASSERT_TRUE(obj.has_mixed_case("Aa"));
+}
+
+
+
+
+
