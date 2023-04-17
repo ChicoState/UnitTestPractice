63c33f67b0e3431d0daad143ef73e49e0c92f0c7
hltiffany@csuchico.edu <hltiffany@csuchico.edu>
2023-04-17 11:52:59 -0700

    x12 mixed case function

--- Password.cpp
+++ Password.cpp
@@ -13,9 +13,43 @@ int Password::count_leading_characters(string phrase){
   int repetition = 1;
   int index = 0;
 
+  if(phrase==""||phrase=="\n")
+  {
+    repetition=0;
+  }
+
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
     repetition++;
     index++;
   }
   return repetition;
+}
+
+bool Password::has_mixed_case(string phrase){
+  bool result=false;
+  bool upper=false;
+  bool lower=false;
+  for(int i=0; i<phrase.size(); i++)
+  {
+    char current=phrase[i];
+    int cur=int(current);
+    if(cur>64)
+    {
+      if(cur<91)
+      {
+        upper=true;
+      }
+    }
+    if(cur>96)
+    {
+      if(cur<123)
+      {
+        lower=true;
+      }
+    }
+    i++;
+  }
+  if(upper && lower)
+    result=true;
+  return result;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -24,3 +24,72 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1,actual);
 }
+
+TEST(PasswordTest, double_letter_password)
+{
+  Password my_password;
+  int actual=my_password.count_leading_characters("aa");
+  ASSERT_EQ(2, actual);
+}
+
+
+TEST(PasswordTest, upper_lower_password)
+{
+  Password my_password;
+  int actual=my_password.count_leading_characters("wW");
+  ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, empty_password)
+{
+  Password my_password;
+  int actual=my_password.count_leading_characters("");
+  ASSERT_EQ(0, actual);
+}
+
+
+TEST(PasswordTest, newline_password)
+{
+  Password my_password;
+  int actual=my_password.count_leading_characters("\n");
+  ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, space_password)
+{
+  Password my_password;
+  int actual=my_password.count_leading_characters("  ");
+  ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, number_password)
+{
+  Password my_password;
+  int actual=my_password.count_leading_characters("1123");
+  ASSERT_EQ(2, actual);
+}
+
+
+TEST(PasswordTest, has_upper_and_lower)
+{
+  Password obj;
+  ASSERT_TRUE(obj.has_mixed_case("Aa"));
+}
+
+TEST(PasswordTest, has_upper)
+{
+  Password obj;
+  ASSERT_FALSE(obj.has_mixed_case("AA"));
+}
+
+TEST(PasswordTest, has_lower)
+{
+  Password obj;
+  ASSERT_FALSE(obj.has_mixed_case("aa"));
+}
+
+TEST(PasswordTest, has_number)
+{
+  Password obj;
+  ASSERT_FALSE(obj.has_mixed_case("123"));
+}
