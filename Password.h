723d425114165e7576ecb1188b35b5b7d7afd86a
Kevin Buffardi <kbuffardi@csuchico.edu>
2023-11-12 11:10:51 -0800

    Add constructor, set, and authenticate member functions

--- Password.h
+++ Password.h
@@ -2,13 +2,15 @@
 #define PRACTICE_H
 
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 class Password
 {
 private:
-
+  vector <string> pass_history;
 public:
   /*
   The function receives a string counts how many times the same character 
@@ -23,5 +25,27 @@ public:
   at least one lower-case letter
   */
   bool has_mixed_case(string);
+
+  /*
+  constructor sets the default password to "ChicoCA-95929"
+  */
+  Password()
+
+  /*
+  receives a password and sets the latest in pass_history to it if and only
+  if it meets all criteria:
+    1. The password is at least 8 letters long
+    2. It has no more than 3 of the same leading characters
+    3. It has mixed case (at least one upper case and at least one lower case)
+    4. It was not a previous password in the history
+  */
+  void set(string);
+
+  /*
+  receives a string and authenticates it against the latest password in the
+  pass_history, returning true for an exact match or false when it does not match
+  or if a password has not been set.
+  */
+  bool authenticate(string);
 };
 #endif
