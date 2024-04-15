5de9323ddefdf6b4e2647c0048acccab45a3c923
Kevin Buffardi <kbuffardi@csuchico.edu>
2024-04-14 19:22:59 -0700

    Add 4 alternative implementations for has_mixed_case

--- Password.cpp
+++ Password.cpp
@@ -18,3 +18,57 @@ int Password::count_leading_characters(string phrase){
   }
   return repetition;
 }
+
+
+// Implementation A
+#include <cctype>
+bool has_mixed_case(string str) {
+    bool has_lower = false;
+    bool has_upper = false;
+    for (char ch : str) {
+        if (std::islower(ch)) {
+            has_lower = true;
+        } else if (std::isupper(ch)) {
+            has_upper = true;
+        }
+    }
+    return has_lower && has_upper;
+}
+// Implementation B
+bool has_mixed_case(string str) {
+    int count_lower = 0;
+    for(int i=0; i<str.length(); i++){
+      if( str[i] >= 'a' && str[i] <= 'z' ){
+        count_lower++;
+      }
+    }
+    return count_lower > 0 && count_lower < str.length();
+}
+// Implementation C
+bool has_mixed_case(string str) {
+    bool lower = false;
+    bool upper = false;
+    for(int i=0; i<str.length(); i++){
+      if( str[i] >= 'a' && str[i] <= 'z' ){
+        lower = true;
+      }
+      else if( str[i] >= 'A' && str[i] <= 'Z'){
+        upper = true;
+      }
+    }
+    return upper && lower;
+}
+// Implementation D
+#include <cctype>
+bool has_mixed_case(string str) {
+    bool is_lower = false;
+    bool is_upper = false;
+    for (char c : str) {
+        if (std::islower(c)) {
+            is_lower = true;
+        } else {
+            is_upper = true;
+        }
+    }
+    return has_lower && has_upper;
+}
