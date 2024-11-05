84099a2a2226703fe043fca6a91f423490be5875
Kevin Buffardi <kbuffardi@csuchico.edu>
2024-11-05 12:35:56 -0800

    Update Password with has_mixed_case placeholder

--- Password.cpp
+++ Password.cpp
@@ -18,3 +18,12 @@ int Password::count_leading_characters(string phrase){
   }
   return repetition;
 }
+
+
+/*
+  receives a string and returns whether it has both at least one upper-case
+  letter and at least one lower-case letter
+*/
+bool Password::has_mixed_case(string pass){
+  return false;
+}
