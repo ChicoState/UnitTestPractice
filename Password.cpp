4330df581b2f309d9782db2f671c2fe25d124c23
Mustafa <cs.mustafaali@gmail.com>
2024-11-13 11:43:06 -0800

    implemented has_mixed_case and creates test cases

--- Password.cpp
+++ Password.cpp
@@ -4,12 +4,15 @@
 using std::string;
 
 /*
-  The function receives a string counts how many times the same character 
+  The function receives a string counts how many times the same character
   occurs at the beginning of the string, before any other characters (or the
   end of the string). The function is case-sensitive so 'Z' is different than
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
+  if(phrase.length() == 0) {
+    return 0;
+  }
   int repetition = 1;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
@@ -25,5 +28,14 @@ int Password::count_leading_characters(string phrase){
   letter and at least one lower-case letter
 */
 bool Password::has_mixed_case(string pass){
-  return false;
+  bool upperCaseExists = false;
+  bool lowerCaseExists = false;
+  for(int i = 0; i < pass.size(); i++) {
+    if(isupper(pass[i])) {
+      upperCaseExists = true;
+    } else if(islower(pass[i])) {
+      lowerCaseExists = true;
+    }
+  }
+  return upperCaseExists && lowerCaseExists;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,11 +5,11 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
+class PasswordTest : public ::testing::Test
 {
 	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
+		PasswordTest(){} //constructor runs before each test
+		virtual ~PasswordTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
@@ -20,3 +20,61 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, double_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZzZ");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, special_characters_leading)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("###Hello");
+    ASSERT_EQ(3, actual);
+}
+
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, space_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters(" ");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, no_case)
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("-@$@#$");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, only_upper)
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("GG");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, only_lower)
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("gg");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, upper_lower)
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("Gg");
+	ASSERT_EQ(true, actual);
+}
+
