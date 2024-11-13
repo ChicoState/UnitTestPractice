7f6ac0061f09c2b22b2bec617f2c9e0b9a573a3a
sokada <sokada1@csuchico.edu>
2024-11-13 11:20:36 -0800

    add method at has_mixed_case

--- Password.cpp
+++ Password.cpp
@@ -10,6 +10,8 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
+  if (phrase.empty()) return 0;
+
   int repetition = 1;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
@@ -25,5 +27,12 @@ int Password::count_leading_characters(string phrase){
   letter and at least one lower-case letter
 */
 bool Password::has_mixed_case(string pass){
+  bool has_upper = false;
+  bool has_lower = false;
+  for (char c : pass) {
+    if (isupper(c)) has_upper = true;
+    if (islower(c)) has_lower = true;
+    if (has_upper && has_lower) return true;
+  }
   return false;
 }
